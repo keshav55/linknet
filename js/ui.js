679cf0a664fbc258d5cdbf3713bd8595c7f34ad8
Anurag Papolu <anurag@Anurags-MacBook-Pro.local>
2015-07-16 00:08:43 -0700

    D

--- js/ui.js
+++ js/ui.js
@@ -1,8 +1,8 @@
 var ref = new Firebase("https://bridgecom.firebaseio.com");
 function authDataCallback(authData) {
 	var authData = ref.getAuth();
-	console.log("authData");
 	if($(".l_o").length == 0 || $(".l_i").length == 0) {
+		alert("TE");
 		location.reload();
 	}
 	if (authData) {
@@ -15,61 +15,25 @@ function authDataCallback(authData) {
 }
 ref.onAuth(authDataCallback);
 
-
-$("#org #register").click(function(){
+$("#register").click(function(){
 	$(this).addClass('disabled');
 	$(this).html('<i class="material-icons">query_builder</i>');
-	var regemail = $("#org #regemail").val();
-	var regpass = $("#org #regpass").val();
+	var regemail = $("#regemail").val();
+	var regpass = $("#regpass").val();
 	ref.createUser({
 		email    : regemail,
 		password : regpass
 	}, function(error, userData) {
 		if (error) {
-			$("#org #regtitle").fadeIn();
-			$("#org #register").removeClass('disabled');
-			$("#org #register").html("Register");
+			$("#regtitle").fadeIn();
+			$("#register").removeClass('disabled');
+			$("#register").html("Register");
 		} else {
 			ref.authWithPassword({
 				email    : regemail,
 				password : regpass
 			}, function(error, authData) {
-				ref.child("users").child(authData.uid).set({
-				  type: "org",
-				  email: authData.email
-				}, function(){
-					location.reload();
-				});				
-				
-			});
-		}
-	});
-});
-$("#vol #register").click(function(){
-	$(this).addClass('disabled');
-	$(this).html('<i class="material-icons">query_builder</i>');
-	var regemail = $("#vol #regemail").val();
-	var regpass = $("#vol #regpass").val();
-	ref.createUser({
-		email    : regemail,
-		password : regpass
-	}, function(error, userData) {
-		if (error) {
-			$("#vol #regtitle").fadeIn();
-			$("#vol #register").removeClass('disabled');
-			$("#vol #register").html("Register");
-		} else {
-			ref.authWithPassword({
-				email    : regemail,
-				password : regpass
-			}, function(error, authData) {
-				ref.child("users").child(authData.uid).set({
-				  type: "vol",
-				  email: authData.email
-				}, function(){
-					location.reload();
-				});				
-				
+				location.reload();
 			});
 		}
 	});
@@ -92,43 +56,15 @@ $("#login").click(function(){
 		}
 	});
 });
-$(".fb_org").click(function(){
+$(".fb").click(function(){
 	$(this).addClass('disabled');
 	$(this).html('<i class="material-icons">query_builder</i>');
 
 	ref.authWithOAuthPopup("facebook", function(error, authData) {
 		if (error) {
-			$("#org .fb_title").fadeIn();
-			$("#org .fb").removeClass('disabled');
-			$("#org .fb").html('<i class="fa fa-facebook"></i>Log in with Facebook');	    
-		} else if(authData && isNewUser) {
-				ref.child("users").child(authData.uid).set({
-				  type: "org",
-				  email: authData.email
-				}, function(){
-					location.reload();
-				});	
-		} else {
-			location.reload();
-		}
-	});
-});
-$(".fb_vol").click(function(){
-	$(this).addClass('disabled');
-	$(this).html('<i class="material-icons">query_builder</i>');
-
-	ref.authWithOAuthPopup("facebook", function(error, authData) {
-		if (error) {
-			$("#vol .fb_title").fadeIn();
-			$("#vol .fb").removeClass('disabled');
-			$("#vol .fb").html('<i class="fa fa-facebook"></i>Log in with Facebook');	    
-		} else if(authData && isNewUser) {
-				ref.child("users").child(authData.uid).set({
-				  type: "vol",
-				  email: authData.email
-				}, function(){
-					location.reload();
-				});	
+			$(".fb_title").fadeIn();
+			$(".fb").removeClass('disabled');
+			$(".fb").html('<i class="fa fa-facebook"></i>Log in with Facebook');	    
 		} else {
 			location.reload();
 		}

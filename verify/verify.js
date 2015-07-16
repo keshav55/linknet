5b863620de518da3683e1b812edecab2a5b8ffcb
Anurag Papolu <anurag@Anurags-MacBook-Pro.local>
2015-07-16 03:29:51 -0700

    D

--- verify/verify.js
+++ verify/verify.js
@@ -21,9 +21,8 @@ function authDataCallback(authData) {
 
 ref.onAuth(authDataCallback);
 $("#submit").click(function(){
-		    ref.child("users").child(ref.getAuth().uid).set({
-		      type: $("input[name=group1]:checked").val()
-		    },function(){
+		    ref.child("users").child(ref.getAuth().uid).push('type': $("input[name=group1]:checked").val()),
+		    function(){
 		    	window.location.replace("http://keshav55.github.io/linknet");
 		    });	
 });

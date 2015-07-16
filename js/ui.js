8dbcbe1afb9e37ed66e9ba62c999c84e197757b0
Anurag Papolu <anurag@Anurags-MacBook-Pro.local>
2015-07-15 21:43:26 -0700

    D

--- js/ui.js
+++ js/ui.js
@@ -1,5 +1,66 @@
+var ref = new Firebase("https://bridgecom.firebaseio.com");
+function authDataCallback(authData) {
+	var authData = ref.getAuth();
+	if (authData) {
+	  $("#reg").remove();
+	  $(".log").remove();
+	  $(".logout").fadeIn();
+	} else {
+	  $(".logout").remove();
+	  $(".log").fadeIn();
+	  $("#reg").slideDown();
+	}
+}
+ref.onAuth(authDataCallback);
+
+$("#register").click(function(){
+	$(this).addClass('disabled');
+	$(this).html('<i class="material-icons">query_builder</i>');
+	var regemail = $("#regemail").val();
+	var regpass = $("#regpass").val();
+	ref.createUser({
+	  email    : regemail,
+	  password : regpass
+	}, function(error, userData) {
+	  if (error) {
+	    $("#regtitle").fadeIn();
+	    $("#register").removeClass('disabled');
+	    $("#register").html("Register");
+	  } else {
+			ref.authWithPassword({
+			  email    : regemail,
+			  password : regpass
+			}, function(error, authData) {
+			  location.reload();
+			});
+	  }
+	});
+});
+$("#login").click(function(){
+	$(this).addClass('disabled');
+	$(this).html('<i class="material-icons">query_builder</i>');
+	var logemail = $("#logemail").val();
+	var logpass = $("#logpass").val();
+	ref.authWithPassword({
+	  email    : logemail,
+	  password : logpass
+	}, function(error, userData) {
+	  if (error) {
+	    $("#logtitle").fadeIn();
+	    $("#login").removeClass('disabled');
+	    $("#login").html("Log In");	    
+	  } else {
+	  	location.reload();
+	  }
+	});
+});
+$(".logout").click(function(){
+	ref.unauth(function(){
+		location.reload();
+	});
+});
+
   $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal-trigger').leanModal();
   });
-          

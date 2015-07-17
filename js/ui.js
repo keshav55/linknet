b7ae7ae7f0eaacb733d0d62ea4b0e13a92ceb06a
Anurag Papolu <anurag@Anurags-MacBook-Pro.local>
2015-07-16 17:48:21 -0700

    D

--- js/ui.js
+++ js/ui.js
@@ -135,4 +135,5 @@ $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal-trigger').leanModal();
     $("a[href='post']").hide();
+    $("a[href='../post']").hide();
 });

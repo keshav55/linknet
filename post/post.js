ec6b1d5ea2a9b31b12ec2c892ccaf9e96809b9fe
Anurag Papolu <anurag@Anurags-MacBook-Pro.local>
2015-07-17 22:38:10 -0700

    D

--- post/post.js
+++ post/post.js
@@ -62,3 +62,39 @@ $(".logout").click(function(){
 		location.reload();
 	});
 });
+$('#startDate').datetimepicker({
+	onClose: function(dateText, inst) {
+		var endDateTextBox = $('#endDate');
+		if (endDateTextBox.val() != '') {
+			var testStartDate = new Date(dateText);
+			var testEndDate = new Date(endDateTextBox.val());
+			if (testStartDate > testEndDate)
+				endDateTextBox.val(dateText);
+		}
+		else {
+			endDateTextBox.val(dateText);
+		}
+	},
+	onSelect: function (selectedDateTime){
+		var start = $(this).datetimepicker('getDate');
+		$('#endDate').datetimepicker('option', 'minDate', new Date(start.getTime()));
+	}
+});
+$('#endDate').datetimepicker({
+	onClose: function(dateText, inst) {
+		var startDateTextBox = $('#startDate');
+		if (startDateTextBox.val() != '') {
+			var testStartDate = new Date(startDateTextBox.val());
+			var testEndDate = new Date(dateText);
+			if (testStartDate > testEndDate)
+				startDateTextBox.val(dateText);
+		}
+		else {
+			startDateTextBox.val(dateText);
+		}
+	},
+	onSelect: function (selectedDateTime){
+		var end = $(this).datetimepicker('getDate');
+		$('#startDate').datetimepicker('option', 'maxDate', new Date(end.getTime()) );
+	}
+});
